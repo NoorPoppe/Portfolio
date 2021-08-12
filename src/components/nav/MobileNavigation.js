a9c75f2b790728bf1413255b490bad66f278863a
NoorPoppe <noor.poppe@student.howest.be>
2021-08-12 14:36:53 +0200

    navabar

--- src/components/nav/MobileNavigation.js
+++ src/components/nav/MobileNavigation.js
@@ -1,15 +1,25 @@
 import styles from "../style/navigation.module.css";
 import NavLinks from "./NavLinks";
 import {CgMenu} from "react-icons/cg"
+import { CgClose } from "react-icons/cg"
+import { useState } from "react";
 
 const MobileNav = () => {
+    const [open, setOpen] = useState(false);
+    const hamburgerIcon = <CgMenu className={styles.hamburger}
+                            size='40px' color='white'
+                            onClick={() => setOpen(!open)}
+                          />;
+    const closeIcon = <CgClose className={styles.hamburger}
+        size='40px' color='white'
+        onClick={() => setOpen(!open)}
+    />;
+
+    const closeMobileMenu = () => setOpen(false);
     return ( 
         <nav className={styles.mobileNavigation}>
-            <CgMenu className={styles.hamburger} 
-                sizes='60px' color='white'
-                onClick={() => console.log("you clicked")}
-            />
-            <NavLinks />
+            {open ? closeIcon : hamburgerIcon}
+            {open && <NavLinks isMobile={true} closeMobileMenu={closeMobileMenu} />}
         </nav>
      );
 }
--- src/components/nav/NavLinks.js
+++ src/components/nav/NavLinks.js
@@ -1,18 +1,24 @@
 import { NavLink } from "react-router-dom";
 import { ROUTES } from "../../consts";
 import logo from "../../home-logo.svg";
+import styles from "../style/navigation.module.css";
+//import {motion} from 'framer-motion'
 
-const NavLinks = () => {
-    return ( 
-        <ul>
-            <li><NavLink exact to={ROUTES.HOME}> <img src={logo} alt="home" /> </NavLink></li>
-            <div >
-                <li><NavLink to={ROUTES.ABOUT} >About</NavLink></li>
-                <li><NavLink to={ROUTES.PROJECTS} >Projects</NavLink></li>
-                <li><NavLink to={ROUTES.CONTACT} >Contact</NavLink></li>
+const NavLinks = (props) => {
+    //const animationFrom = {opacity: 0, y:-40};
+    //const animationTo = { opacity: 1, y: 0 };
+
+    return (
+        <ul className={styles.nav}>
+            <li className={styles.home__ham}><NavLink exact to={ROUTES.HOME}> <img src={logo} alt="home" /> </NavLink></li>
+            <div className={styles.nav__div}>
+                <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink className={`${styles.home} ${styles.nav__li}`} to={ROUTES.HOME} >Home</NavLink></li>
+                <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink className={styles.nav__li} to={ROUTES.ABOUT} >About</NavLink></li>
+                <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink className={styles.nav__li} to={ROUTES.PROJECTS} >Projects</NavLink></li>
+                <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink className={styles.nav__li} to={ROUTES.CONTACT} >Contact</NavLink></li>
             </div>
         </ul>
-     );
+    );
 }
- 
+
 export default NavLinks;
